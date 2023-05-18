48aa0cd2d3a9829c807627a3f573bf178ccd9e9e
Jihye Yang <s2127@e-mirim.hs.kr>
2023-05-18 10:24:26 +0900

    값 넘기기

---
+++ src/main/java/kr/hs/study/ValuePass/ValuePassApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.study.ValuePass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValuePassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValuePassApplication.class, args);
+	}
+
+}
---
+++ src/main/java/kr/hs/study/ValuePass/controller/testController.java
@@ -0,0 +1,19 @@
+package kr.hs.study.ValuePass.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class testController {
+    @GetMapping("/test1")
+    public String test1(HttpServletRequest req){
+        String d1= req.getParameter("data1");
+        String d2= req.getParameter("data2");
+
+        System.out.println("data1 : "+d1);
+        System.out.println("data2 : "+d2);
+        return "result";
+    }
+}
